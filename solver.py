a49f3f564e0759a1e73c341816626224a5c8d78a
Om Patel <opat597@aucklanduni.ac.nz>
2024-07-22 20:10:08 +1200

    Created validity checker to see if inserted number is valid

--- solver.py
+++ solver.py
@@ -10,9 +10,45 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range (1,10):
+        if valid(board, i, (row, col)):
+
+    
+
+def is_valid(board, number, position):
+    
+    # Check row
+    for i in range (len(board[0])):
+        if board[position[0]][i] == num and pos[1] != i:
+            return False
+        
+    # Check coloumn
+    for i in range (len(board)):
+        if board[i][position[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range (box_y * 3, box_y * 3 + 3):
+        for j in range (box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i,j) != position:
+                return False
+    
+    return True
+ 
 def print_board(board):
     
-    for i in range(len(board)):
+    for i in range(len(board)): 
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
@@ -26,4 +62,10 @@ def print_board(board):
                 print(str(board[i][j]) + " ", end="")
             
 
-print_board(board)
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    
+    return None
