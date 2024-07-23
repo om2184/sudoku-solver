c5fd0dedea5394c583b268505cc59a53642957f9
Om Patel <opat597@aucklanduni.ac.nz>
2024-07-23 16:10:49 +1200

    Restructuring code to use multiple classes and reusable methods

--- GUI.py
+++ GUI.py
@@ -1,35 +1,102 @@
 import pygame
 import requests
+from solver import solve, is_valid
 
-response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
-grid = response.json()['board']
-grid_original = [[grid[x][y] for y in range (len(grid[0]))] for x in range (len(grid))]
 
-WIDTH = 550
-background_colour = (255, 255, 255)
-original_grid_colour = (52, 35, 151)
+
+
+class Grid:
+
+    board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+    
+    def _init_(self, rows, cols, width, height, win, board):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.win = win
+        self.selected = None
+
+    def draw(self):
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(self.win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(self.win, gap)
+
+        
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def _init_(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+
+    def draw(self, win, gap):
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            font = pygame.font.SysFont("Arial", 25)
+            text = font.render(str(self.temp), 1, (128,128,128))
+            win.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            font = pygame.font.SysFont("Arial", 35)
+            text = font.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+
+    
+    def set(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
+
 
 
 def main():
     pygame.init()
-    win = pygame.display.set_mode((WIDTH, WIDTH))
+    win = pygame.display.set_mode((550, 600))
     pygame.display.set_caption("Sudoku")
-    win.fill(background_colour)
+    board = Grid(9, 9, 540, 540, win)
     myfont = pygame.font.SysFont("Comic Sans MS", 35)
-
-    for i in range(0,10):
-        if(i%3 == 0):
-            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), ((50 + 50*i, 500)), 4)
-            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), ((500, 50 + 50*i)), 4)
-
-        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), ((50 + 50*i, 500)), 2)
-        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), ((500, 50 + 50*i)), 2)
-    pygame.display.update()
-
-    for i in range (0, len(grid[0])):
-        for j in range (0, len(grid[0])):
-            if(0<grid[i][j]<10):
-                value = myfont.render(str(grid[i][j]), True, original_grid_colour)
 
 
     while True:
